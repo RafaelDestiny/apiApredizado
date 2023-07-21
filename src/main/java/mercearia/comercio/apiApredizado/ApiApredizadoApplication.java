23c6d2aea0d93bbeaf213441215e8f1de0429d51
rafaeldestiny1984@gmail.com <rafaeldestiny1984@gmail.com>
2023-07-21 11:33:02 -0300

    Project created

---
+++ src/main/java/mercearia/comercio/apiApredizado/ApiApredizadoApplication.java
@@ -0,0 +1,13 @@
+package mercearia.comercio.apiApredizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApredizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApredizadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mercearia/comercio/apiApredizado/ApiApredizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package mercearia.comercio.apiApredizado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApredizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
